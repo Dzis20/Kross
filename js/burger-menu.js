7aaa27d065750c5fb6e06f602164c0ea57aad8ff
Dzis <Dzis99@yandex.ru>
2020-10-29 23:15:31 +0500

    Изменение структуры проекта

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//     $('.burger-menu').click(function(event) {
+//         $('.burger-menu,.slide-menu').toggleClass('active');
+//         $('body').toggleClass('lock');
+//     });
+//  });
     $('.burger-menu').click(function(event) {
-        $('.burger-menu,.slide-menu').toggleClass('active');
+        $('.burger-menu,.slide-menu,.wrapper').toggleClass('active');
         $('body').toggleClass('lock');
     });
- });
-
